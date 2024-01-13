2d3262090fbfd917170c1410695764f136cb175d
p3lo <mysiacik@mysiacik.com>
2024-01-14 00:03:49 +0100

    Remove ESLint configuration and update package.json and remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,15 +1,18 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
   serverDependenciesToBundle: [
     'swiper',
     'swiper/react',
+    'swiper/modules',
     'swiper/swiper-bundle.css',
+    'remix-utils/client-only',
     'ssr-window',
     'marked',
     'react-leaflet',
     '@react-leaflet/core',
   ],
   ignoredRouteFiles: ['**/.*'],
+  serverModuleFormat: 'cjs',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
